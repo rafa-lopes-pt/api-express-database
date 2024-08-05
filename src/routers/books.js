ef083a8a2130134d864ebb58ae6e6c6cf0e9ab9e
Rafa Lopes <rafalopessecond@gmail.com>
2024-08-05 02:27:19 +0100

    endpoints

--- src/routers/books.js
+++ src/routers/books.js
@@ -2,7 +2,19 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db");
 
+router.post('/', async (req, res) => {
+
+})
 router.get('/', async (req, res) => {
+
+})
+router.get('/:id', async (req, res) => {
+
+})
+router.put('/', async (req, res) => {
+
+})
+router.delete('/', async (req, res) => {
 
 })
 
